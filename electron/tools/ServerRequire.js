c7028aaf9a6d6645cf22445a6ac92483061c2b6b
Lao-Qi <2231675321@qq.com>
2022-07-03 17:01:45 +0800

    规范本地请求接口方法的定义

--- electron/tools/ServerRequire.js
+++ electron/tools/ServerRequire.js
@@ -8,23 +8,28 @@ const UserStore = new Store()
 // 获取本地存储中的token;
 const localToken = UserStore.get("token")
 
-// 获取服务端公钥
+/**
+ * @example 获取服务器公钥
+ * @returns { Promise<String | Boolean >}
+ */
 async function GetPublicKey() {
     const res = await axios.get("/api/publicKey")
-    return res.status === 200 && res.data.code === 200
-        ? Buffer.from(res.data.publicKey, "base64").toString()
-        : false
+    return res.status === 200 && res.data.code === 200 ? Buffer.from(res.data.publicKey, "base64").toString() : false
 }
 
-// 注册用户
+/**
+ * @example 注册用户
+ * @param { String } name
+ * @param { String } account
+ * @param { String } paw
+ * @returns { Promise<Object | Boolean >}
+ */
 async function RegisterUser(name, account, paw) {
     const publicKey = await GetPublicKey()
     if (publicKey.status !== 200) {
         return publicKey.status
     } else {
-        const ping = publicEncrypt(publicKey, Buffer.from(paw)).toString(
-            "base64"
-        )
+        const ping = publicEncrypt(publicKey, Buffer.from(paw)).toString("base64")
         const res = await axios.post("/api/user/register", {
             name,
             account,
@@ -35,21 +40,31 @@ async function RegisterUser(name, account, paw) {
     }
 }
 
-// 登录用户生成token
+/**
+ * @example 用户登录
+ * @param { String } account
+ * @param { String } paw
+ * @returns { Promise<Object | Boolean >}
+ */
 async function LoginUser(account, paw) {
     const publicKey = await GetPublicKey()
     if (!publicKey) {
-        return publicKey
+        return {
+            code: 404,
+            msg: "网络请求失败，请检查本地网络连接状态",
+        }
     } else {
-        const ping = publicEncrypt(publicKey, Buffer.from(paw)).toString(
-            "base64"
-        )
+        console.log(account, paw)
+        const ping = publicEncrypt(publicKey, Buffer.from(paw)).toString("base64")
         const res = await axios.post(`/api/user/login`, { account, ping })
         return res.status === 200 ? res.data : false
     }
 }
 
-// 查询当前用户的好友列表
+/**
+ * @example 查询当前登录用户的好友列表
+ * @returns { Promise<Object | Boolean> }
+ */
 async function FindUserFriendsList() {
     const res = await axios.get("/api/search/FriendsList", {
         // token用户的身份认证
@@ -58,7 +73,11 @@ async function FindUserFriendsList() {
     return res.status === 200 ? res.data : false
 }
 
-// 添加好友
+/**
+ * @example 添加好友
+ * @param { String } account
+ * @returns { Promise<Object | Boolean }
+ */
 async function AddFriend(account) {
     const res = await axios.post(
         "/api/user/addFriend",
@@ -70,7 +89,11 @@ async function AddFriend(account) {
     return res.status === 200 ? res.data : false
 }
 
-// 通过账号查询用户
+/**
+ * @example 通过账号查询用户
+ * @param { String } account
+ * @returns { Promise<Object | Boolean>}
+ */
 async function SearchUser(account) {
     const res = await axios.get("/api/search/searchUser", {
         params: { account },
@@ -78,7 +101,11 @@ async function SearchUser(account) {
     return res.status === 200 ? res.data : false
 }
 
-// 查询用户
+/**
+ * @example 模糊查询用户
+ * @param { String } key
+ * @returns { Promise<Object | Boolean>}
+ */
 async function SearchUsers(key) {
     const res = await axios.get("/api/search/SearchUsers", { params: { key } })
     return res.status === 200 ? res.data : false
