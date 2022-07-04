ed3debaff8fea1ce41e5d0a02fd652771637d8bf
Lao-Qi <2231675321@qq.com>
2022-07-04 22:02:25 +0800

    添加一个智能的打开对应窗口的功能

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,9 @@
 "use strict"
 const { app, ipcMain } = require("electron")
+const Store = require("electron-store")
 const createLoginWin = require("./windows/loginWin")
 const createMainWin = require("./windows/mainWin")
+const { VerifyTokenIsOut } = require("./tools/ServerRequire")
 
 let loginWin = null, // 登录窗口
     mainWin = null, // 主窗口
@@ -10,20 +12,30 @@ let loginWin = null, // 登录窗口
 // let currentWinName = null
 
 app.whenReady().then(async () => {
-    loginWin = await createLoginWin()
-    currentWin = loginWin
-    // currentWinName = "loginWin"
-    loginWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-    loginWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
-
-    ipcMain.on("login-complete-open-mainWin", () => {
-        loginWin.close()
+    const UserStore = new Store({ accessPropertiesByDotNotation: false })
+    const token = UserStore.get("token")
+    // token存在且没过期
+    if (token && VerifyTokenIsOut(token)) {
         mainWin = createMainWin()
         currentWin = mainWin
-        // currentWinName = "mainWin"
         mainWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
         mainWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
-    })
+    } else {
+        loginWin = await createLoginWin()
+        currentWin = loginWin
+        // currentWinName = "loginWin"
+        loginWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+        loginWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+
+        ipcMain.on("login-complete-open-mainWin", () => {
+            loginWin.close()
+            mainWin = createMainWin()
+            currentWin = mainWin
+            // currentWinName = "mainWin"
+            mainWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+            mainWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+        })
+    }
 
     ipcMain.on("window-minimize", () => currentWin.minimize())
     ipcMain.on("window-maximize", () => (currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()))
