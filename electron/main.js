21ea1964b8b0b48ebefec796694c7a7b0c657ab6
Lao-Qi <2231675321@qq.com>
2022-07-04 13:18:19 +0800

    修改在主页下窗口放大和复原时字体图标不会改变的问题

--- electron/main.js
+++ electron/main.js
@@ -7,21 +7,25 @@ let loginWin = null, // 登录窗口
     mainWin = null, // 主窗口
     currentWin = null // 当前页面上的窗口
 
+// let currentWinName = null
+
 app.whenReady().then(async () => {
     loginWin = await createLoginWin()
     currentWin = loginWin
+    // currentWinName = "loginWin"
+    loginWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+    loginWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
 
     ipcMain.on("login-complete-open-mainWin", () => {
         loginWin.close()
         mainWin = createMainWin()
         currentWin = mainWin
+        // currentWinName = "mainWin"
+        mainWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+        mainWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
     })
 
     ipcMain.on("window-minimize", () => currentWin.minimize())
-    ipcMain.on("window-maximize", () => {
-        currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()
-    })
+    ipcMain.on("window-maximize", () => (currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()))
     ipcMain.on("window-destroy", () => currentWin.destroy())
-    currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-    currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
 })
