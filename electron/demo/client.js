b6b685954366243e10b341eb5f9553a640070be3
Lao-Qi <2231675321@qq.com>
2022-07-07 21:44:41 +0800

    更新测试案例

--- electron/demo/client.js
+++ electron/demo/client.js
@@ -1,9 +1,8 @@
 const io = require("socket.io-client")
-const { GetPublicKey, LoginUser } = require("../tools/ServerRequire")
+const { LoginUser } = require("../tools/ServerRequire")
 
 async function main() {
-    const publicKey = await GetPublicKey()
-    const LoginData = await LoginUser("12301276817", "12301276817", publicKey)
+    const LoginData = await LoginUser("12301276817", "12301276817")
     const socket = io.connect("http://127.0.0.1:6001", {
         auth: {
             token: LoginData.token,
@@ -11,13 +10,14 @@ async function main() {
         timeout: 4000,
     })
     socket.on("connect", () => {
-        socket.emit("join room", "2231675321")
-        socket.emit("send message", "在吗")
+        console.log("连接成功")
+        // socket.emit("join room", "2231675321")
+        // socket.emit("send message", "在吗")
     })
 
-    socket.on("message", (msg) => {
-        console.log(msg)
-    })
+    // socket.on("message", (msg) => {
+    //     console.log(msg)
+    // })
 }
 
 main().then()
