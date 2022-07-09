e77e561a0d3f25d84a47c3e718567d00bb6a9d5a
Lao-Qi <2231675321@qq.com>
2022-07-09 17:03:16 +0800

    修改打包页面资源打包配置

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,17 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
-import { ElementPlusResolver, VueUseComponentsResolver } from "unplugin-vue-components/resolvers"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue({
-            include: [/\.vue$/, /\.md$/],
-        }),
+        vue(),
         AutoImport({
-            resolvers: [ElementPlusResolver(), VueUseComponentsResolver()],
-            imports: [
-                "vue",
-                "vue-router",
-                {
-                    "element-plus": ["ElTooltip", "ElNotification"],
-                },
-            ],
+            resolvers: [ElementPlusResolver()],
         }),
         Components({
-            extensions: ["vue", "md", "svg"],
-            directoryAsNamespace: true,
-            dts: true,
-            resolvers: [
-                ElementPlusResolver(),
-                VueUseComponentsResolver(),
-                componentName => {
-                    if (componentName.startsWith("Icon")) {
-                        return { name: componentName.slice(4), from: "@icon-park/vue-next" }
-                    }
-                },
-            ],
+            resolvers: [ElementPlusResolver()],
         }),
     ],
     base: "./",
