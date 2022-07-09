d1be208d528d98bced4529e071fb7887a2f886e0
Lao-Qi <2231675321@qq.com>
2022-07-09 16:32:19 +0800

    给标题加上logo

--- src/components/title/windows-title.vue
+++ src/components/title/windows-title.vue
@@ -1,10 +1,11 @@
 <template>
-    <div id="logo"></div>
+    <div id="logo">
+        <img src="../../assets/icons/tibook-login.svg" alt="题书" />
+    </div>
     <p></p>
     <div class="window-controls-container">
         <div class="window-icon window-minimize" @click="() => ipcRenderer.send('window-minimize')"></div>
-        <div class="window-icon" :class="isMaximize ? 'window-unmaximize' : 'window-maximize'" ref="maximizeBtn"
-            @click="() => ipcRenderer.send('window-maximize')"></div>
+        <div class="window-icon" :class="isMaximize ? 'window-unmaximize' : 'window-maximize'" ref="maximizeBtn" @click="() => ipcRenderer.send('window-maximize')"></div>
         <div class="window-icon window-close" @click="MainWinDestroy"></div>
     </div>
 </template>
@@ -14,8 +15,8 @@ import { ref } from "vue"
 const { ipcRenderer } = require("electron")
 
 const isMaximize = ref(false)
-ipcRenderer.on("window-maximize", () => isMaximize.value = true)
-ipcRenderer.on("window-unmaximize", () => isMaximize.value = false)
+ipcRenderer.on("window-maximize", () => (isMaximize.value = true))
+ipcRenderer.on("window-unmaximize", () => (isMaximize.value = false))
 
 function MainWinDestroy() {
     ipcRenderer.send("window-destroy")
@@ -27,6 +28,11 @@ function MainWinDestroy() {
 #logo {
     width: 30px;
     height: 100%;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
 }
 
 p {
