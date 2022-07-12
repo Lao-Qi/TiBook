a5183a05010a41939bb5e3d42bf5c9742709cb34
Lao-Qi <2231675321@qq.com>
2022-07-12 13:47:01 +0800

    优化入口文件代码

--- electron/main.js
+++ electron/main.js
@@ -5,45 +5,41 @@ const Store = require("electron-store")
 
 // 配置页面资源加载环境
 process.env.LoadPath = process.env?.IS_DEV ? "http://127.0.0.1:3000" : `file:///${join(__dirname, "../dist/index.html")}`
+let currentWin = null,
+    currentWinName = null
 
-const createLoginWin = require("./windows/loginWin")
-const createMainWin = require("./windows/mainWin")
-const { VerifyTokenIsOut } = require("./tools/ServerRequire")
-
-let loginWin = null, // 登录窗口
-    mainWin = null, // 主窗口
-    currentWin = null, // 当前窗口
-    currentWinName = null // 当前窗口的名称
+// 不使用硬件加速渲染软件，会导致软件有残影
+app.disableHardwareAcceleration()
 
 app.whenReady().then(async () => {
     const UserStore = new Store({ accessPropertiesByDotNotation: false })
-    const token = UserStore.get("token")
-    // token存在且没过期
-    if (token && VerifyTokenIsOut(token)) {
-        mainWin = createMainWin()
-        currentWin = mainWin
+    // token存在
+    if (UserStore.get("token") && false) {
+        currentWin = require("./windows/mainWin")()
         currentWinName = "main"
-        mainWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-        mainWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+        currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+        currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
     } else {
-        loginWin = await createLoginWin()
-        currentWin = loginWin
+        currentWin = await require("./windows/loginWin")()
         currentWinName = "login"
-        loginWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-        loginWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+        currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+        currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
 
         ipcMain.on("login-complete-open-mainWin", () => {
-            loginWin.close()
-            mainWin = createMainWin()
-            currentWin = mainWin
+            currentWin.close()
+            currentWin = null
+            currentWin = require("./windows/mainWin")()
             currentWinName = "main"
-            mainWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-            mainWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+            currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+            currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
         })
     }
 
+    ipcMain.handle("get-current-page", () => currentWinName)
     ipcMain.on("window-minimize", () => currentWin.minimize())
     ipcMain.on("window-maximize", () => (currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()))
-    ipcMain.on("window-destroy", () => currentWin.destroy())
-    ipcMain.handle("get-current-page", () => currentWinName)
+    ipcMain.on("window-destroy", () => {
+        currentWin.destroy()
+        currentWin = null
+    })
 })
