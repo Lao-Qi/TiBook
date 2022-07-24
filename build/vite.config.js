6576332942a8b3d3360260b5704aab2c698f7e8d
Lao-Qi <2231675321@qq.com>
2022-07-24 13:35:32 +0800

    移动public文件夹位置

--- build/vite.config.js
+++ build/vite.config.js
@@ -5,6 +5,7 @@ import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 export default defineConfig({
+    publicDir: "../public",
     plugins: [
         vue(),
         AutoImport({
