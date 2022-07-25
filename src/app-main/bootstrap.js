c84e9f49a5d58c3454c967e0df7bee7f370a508b
Lao-Qi <2231675321@qq.com>
2022-07-25 19:19:20 +0800

    优化软件启动过程(未完工)

---
+++ src/app-main/bootstrap.js
@@ -0,0 +1,67 @@
+/**
+ * 软件的启动页
+ *
+ * 用来启动窗口，启动其他服务
+ *
+ * 这个文件在app前置内容加载好后开始加载
+ */
+
+const { BrowserWindow, ipcMain } = require("electron")
+const { createBrowserService } = require("./lib/BrowserService")
+const { join } = require("path")
+
+/**
+ * 第一步当然是启动窗口，加载软件页面的整体ui框架
+ */
+startWindow()
+
+/**
+ * 启动服务线程
+ */
+startServices()
+
+async function startWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 600,
+        minHeight: 450,
+        show: false,
+        frame: false,
+        useContentSize: true,
+        webPreferences: {
+            // 安全的沙盒模式是需要的
+            sandbox: true,
+            // 需要使用node的api，嗯...最少得可以导入electron的渲染进程工具
+            nodeIntegration: true,
+            contextIsolation: true
+        }
+    })
+
+    // 加载入口文件就配置好了的地址
+    win.loadURL(process.env["TIBOOK_APP_PAGR_URL"])
+    win.once("ready-to-show", () => win.show())
+
+    // 关闭就清楚相关事件
+    win.once("close", () => {
+        ipcMain.removeAllListeners(["window-minimize", "window-maximize", "window-destroy"])
+    })
+
+    // 窗口事件转接到前端事件
+    win.on("enter-full-screen", () => win.webContents.send("window-enter-full-screen"))
+    win.on("leave-full-screen", () => win.webContents.send("window-leave-full-screen"))
+    win.on("enter-html-full-screen", () => win.webContents.send("window-enter-full-screen"))
+    win.on("leave-html-full-screen", () => win.webContents.send("window-leave-full-screen"))
+
+    // 绑定窗口的控件信息
+    ipcMain.on("window-minimize", () => win.minimize())
+    ipcMain.on("window-maximize", () => (win.isMaximized() ? win.unmaximize() : win.maximize()))
+    ipcMain.once("window-destroy", () => win.close())
+}
+
+async function startServices() {
+    /**
+     * 创建一个服务线程，并加载服务
+     */
+    createBrowserService(join(__dirname, "./services-entrance.js"), "services")
+}
--- src/app-main/main.js
+++ src/app-main/main.js
@@ -1,45 +1,54 @@
 "use strict"
 const { join } = require("path")
-const { app, ipcMain } = require("electron")
-const Store = require("electron-store")
+const { app, protocol } = require("electron")
 
-// 配置页面资源加载环境
-process.env.LoadPath = process.env?.IS_DEV ? "http://127.0.0.1:3000" : `file:///${join(__dirname, "../dist/index.html")}`
-let currentWin = null,
-    currentWinName = null
+// let currentWin = null,
+//     currentWinName = null
 
 // 不使用硬件加速渲染软件，会导致软件有残影
 app.disableHardwareAcceleration()
 
-app.once("ready", async () => {
-    const UserStore = new Store({ accessPropertiesByDotNotation: false })
-    // token存在
-    if (UserStore.get("token")) {
-        currentWin = require("./windows/mainWin")()
-        currentWinName = "main"
-        currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-        currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
-    } else {
-        currentWin = await require("./windows/loginWin")()
-        currentWinName = "login"
-        currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-        currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
-
-        ipcMain.on("login-complete-open-mainWin", () => {
-            currentWin.close()
-            currentWin = null
-            currentWin = require("./windows/mainWin")()
-            currentWinName = "main"
-            currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
-            currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
-        })
+// 注册一个用来获取本地资源的协议
+protocol.registerSchemesAsPrivileged([
+    {
+        scheme: "tibook-file",
+        privileges: {
+            secure: true,
+            standard: true,
+            supportFetchAPI: true,
+            corsEnabled: true,
+            allowServiceWorkers: true
+        }
     }
+])
 
-    ipcMain.handle("get-current-page", () => currentWinName)
-    ipcMain.on("window-minimize", () => currentWin.minimize())
-    ipcMain.on("window-maximize", () => (currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()))
-    ipcMain.on("window-destroy", () => {
-        currentWin.destroy()
-        currentWin = null
-    })
+// 配置页面资源加载环境
+process.env["TIBOOK_MAIN_PAGR_URL"] = process.env?.IS_DEV ? "http://127.0.0.1:3000" : `file:///${join(__dirname, "../dist/index.html")}`
+
+app.once("ready", () => {
+    require("./bootstrap")
 })
+
+// async function onReady() {
+//     currentWin = await require("./windows/loginWin")()
+//     currentWinName = "login"
+//     currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+//     currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+
+//     ipcMain.on("login-complete-open-mainWin", () => {
+//         currentWin.close()
+//         currentWin = null
+//         currentWin = require("./windows/mainWin")()
+//         currentWinName = "main"
+//         currentWin.on("maximize", () => currentWin.webContents.send("window-maximize"))
+//         currentWin.on("unmaximize", () => currentWin.webContents.send("window-unmaximize"))
+//     })
+
+//     ipcMain.handle("get-current-page", () => currentWinName)
+//     ipcMain.on("window-minimize", () => currentWin.minimize())
+//     ipcMain.on("window-maximize", () => (currentWin.isMaximized() ? currentWin.unmaximize() : currentWin.maximize()))
+//     ipcMain.on("window-destroy", () => {
+//         currentWin.destroy()
+//         currentWin = null
+//     })
+// }
