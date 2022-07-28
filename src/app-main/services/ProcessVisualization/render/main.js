b43abefbe619b277c0cbcc00cea875147fc52f65
Lao-Qi <2231675321@qq.com>
2022-07-28 21:39:03 +0800

    实现进程可视化管理

--- src/app-main/services/ProcessVisualization/render/main.js
+++ src/app-main/services/ProcessVisualization/render/main.js
@@ -2,6 +2,10 @@ const { ipcRenderer } = require("electron")
 
 const exhibitListContainer = document.querySelector(".process_list")
 const currentState = document.querySelector(".menu>span")
+const PROCESS = {
+    AllInfo: null,
+    current: null
+}
 
 // 字段的顺序表
 const FieldOrder = {
@@ -20,15 +24,35 @@ const optionsMenu = {
     reloadAll: "重启全部"
 }
 
-let currentProcess = null
-
 renderOptionMenu(optionsMenu)
 renderFieldTableHead(FieldOrder)
 
 // 从主进程获取进程集合表
 ipcRenderer.invoke("getProcessAllInfo").then(ProcessAllInfo => {
-    Object.values(ProcessAllInfo).forEach(ProcessInfo => renderProcessLi(ProcessInfo))
+    PROCESS.AllInfo = ProcessAllInfo
+    ProcessAllInfo.forEach(processInfo => {
+        renderProcessLi(processInfo)
+    })
 })
+/**
+ * 为了确保数据是响应式更新的，这里的设计是当服务进程启动的时候开启定时器，每一秒就向主进程获取进程数据
+ * 如果有变动就改变数据
+ */
+setInterval(() => {
+    ipcRenderer.invoke("getProcessAllInfo").then(ProcessAllInfo => {
+        PROCESS.AllInfo = ProcessAllInfo
+        ProcessAllInfo.forEach(processInfo => {
+            const MemoryBox = document.querySelector(`li[data-pid="${processInfo.PID}"]>span:nth-last-child(2)`)
+            const CPUBOX = document.querySelector(`li[data-pid="${processInfo.PID}"]>span:nth-last-child(1)`)
+            if (MemoryBox && CPUBOX) {
+                MemoryBox.innerHTML = processInfo["Memory"]
+                CPUBOX.innerHTML = processInfo["CPU"]
+            } else {
+                renderProcessLi(processInfo)
+            }
+        })
+    })
+}, 1000)
 
 /**
  * 渲染进程数据
@@ -42,7 +66,7 @@ function renderProcessLi(processInfo) {
     })
 
     const li = document.createElement("li")
-    li.setAttribute("data-PID", processInfo.PID)
+    li.setAttribute("data-pid", processInfo.PID)
 
     /**
      * const a = [1, 2, 3]
@@ -55,7 +79,6 @@ function renderProcessLi(processInfo) {
      */
     li.innerHTML = `<span title="${infoAry[0]}">${infoAry.join("</span><span>")}</span>`
     exhibitListContainer.appendChild(li)
-
     li.onclick = function () {
         /**
          * 当进程被选中时判断是否和上一个进程一致，如果一致，则取消选中进程
@@ -64,12 +87,16 @@ function renderProcessLi(processInfo) {
          */
         const upCurrentProcess = document.querySelector(".current_process")
         if (upCurrentProcess && upCurrentProcess.getAttribute("data-PID") === processInfo.PID) {
-            li.setAttribute("class", "")
+            this.setAttribute("class", "")
+            PROCESS.current = null
+            currentState.innerHTML = "当前无选中进程"
         } else {
             for (let i = 0; i < exhibitListContainer.children.length; i++) {
                 exhibitListContainer.children[i].setAttribute("class", "")
             }
-            li.setAttribute("class", "current_process")
+            PROCESS.current = processInfo
+            currentState.innerHTML = `选中进程PID: ${PROCESS.current.PID}`
+            this.setAttribute("class", "current_process")
         }
     }
 }
@@ -94,11 +121,12 @@ function renderOptionMenu(optionsMenu) {
          * 如果没有则弹出提示框
          */
         a.onclick = function () {
-            if (currentProcess) {
+            if (PROCESS.current) {
                 const operate = this.getAttribute("data-operate")
-                if (operate !== "reloadAll") {
-                    operateProcess(currentProcess.PID, operate)
-                } else if (currentProcess) {
+                if (operate === "reloadAll") {
+                    ipcRenderer.send("operateProcessAllReload")
+                } else {
+                    ipcRenderer.invoke("operateProcess", PROCESS.current.PID, operate)
                 }
             } else {
                 alert("请选择要操作的进程")
@@ -115,16 +143,13 @@ function renderFieldTableHead(FieldMap) {
     const TableHead = document.querySelector(".process_list_table_head")
     Object.keys(FieldMap).forEach(field => {
         const span = document.createElement("span")
-        span.innerText = field
+        if (field === "Memory") {
+            span.innerText = field + "(MB)"
+        } else if (field === "CPU") {
+            span.innerText = field + "(%)"
+        } else {
+            span.innerText = field
+        }
         TableHead.appendChild(span)
     })
-}
-
-/**
- * 通过进程id联系主进程进行通信
- * @param { String } pid 进程id
- * @param { String } operate 进程实例读写的方法名或属性名
- */
-function operateProcess(pid, operate) {
-    ipcRenderer.invoke("operateProcess", pid, operate)
 }
