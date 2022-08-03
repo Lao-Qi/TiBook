4a478383eb486b236b83b64931f565464dad9b7a
Lao-Qi <2231675321@qq.com>
2022-08-03 11:16:40 +0800

    实现添加本地数据操作

---
+++ src/app-main/lib/LocalDatabase/index.js
@@ -0,0 +1,44 @@
+"use strict"
+/**
+ * 连接本地数据库
+ *
+ * 该软件涉及到的所有本地数据存储
+ 
+ *
+ * 所有的本地会再app触发ready事件后触发
+ * 且该库只会再主进程中被调用
+ *
+ * nedb并不是这个软件的最优数据存储解决方法，可能到后面会使用其他技术来实现更快和占用内存更少且支持跟大数据量
+ */
+const { accessSync, writeFileSync } = require("fs")
+const { join } = require("path")
+const NeDB = require("nedb")
+
+const DBFilesName = {
+    USER_MESSAGE: null,
+    USER_CHATLIST: null
+}
+
+for (const [key, _] of Object.entries(DBFilesName)) {
+    const filePath = join(process.env["TIBOOK_APP_LOCATION"], `${key}.db`)
+
+    /**
+     * 验证文件是否存在
+     */
+    try {
+        accessSync(filePath)
+    } catch {
+        writeFileSync(filePath, "")
+    }
+
+    /**
+     * 生成存储库
+     */
+    DBFilesName[key] = new NeDB({
+        filename: filePath,
+        corruptAlertThreshold: process.env["IS_DEV"] ? 1 : 0,
+        autoload: true
+    })
+}
+
+module.exports = DBFilesName
--- src/app-main/tools/LocalOperation.js
+++ src/app-main/tools/LocalOperation.js
@@ -1,252 +1,155 @@
 "use strict"
-const { accessSync, writeFileSync } = require("fs")
-const path = require("path")
-const Nedb = require("nedb")
-const { app } = require("electron")
-const { SearchUser } = require("./node/ServerRequire")
-
-// 验证本地数据库存储文件是否齐全
-const DBFiles = ["message.db", "messageCards.db", "friends.db"]
-for (let i = 0; i < DBFiles.length; i++) {
-    const dbFilePath = path.join(app.getAppPath(), DBFiles[i])
-    try {
-        accessSync(dbFilePath)
-    } catch {
-        writeFileSync(dbFilePath, "")
+/**
+ * 操作本地数据的工具方法集合文件
+ *
+ * 这个文件会被child_process进行fork开启，然后由消息来决定运行哪个方法
+ *
+ * 这里接收renderProcessMark(渲染进程的标注)是为了判断是那个进程要执行的操作
+ */
+const { USER_CHATLIST, USER_MESSAGE } = require("../lib/LocalDatabase/index")
+process.on("message", ({ request, args, renderProcessMark }) => {
+    if (!LocalOperationMethodAllMap[request]) {
+        throw Error(`本地操作函数不存在: (${request})`)
     }
-}
-// 本地软件存储
-const DB = {
-    message: new Nedb({
-        filename: path.join(app.getAppPath(), "./message.db"), // 文件路径
-        autoload: true, // 自动连接
-        corruptAlertThreshold: 1 // 数据缺失多少报错， 1表示不报错
-    }),
-    friends: new Nedb({
-        filename: path.join(app.getAppPath(), "./friends.db"),
-        autoload: true,
-        corruptAlertThreshold: 1
-    }),
-    messageCards: new Nedb({
-        filename: path.join(app.getAppPath(), "./messageCards.db"),
-        autoload: true,
-        corruptAlertThreshold: 1
-    })
-}
+    LocalOperationMethodAllMap[request](...args)
+        .then(result => {
+            process.send({
+                result,
+                request,
+                state: 0,
+                renderProcessMark
+            })
+        })
+        .catch(result => {
+            process.send({
+                result,
+                request,
+                state: 1,
+                renderProcessMark
+            })
+        })
+})
 
-/**
- * @example 向本地存储中插入新的消息
- * @param { Object } msg
- * @param { Boolean } isRead
- */
-function insertMessage(msg, isRead) {
-    DB.message.insert({ ...msg, isRead })
-}
+const LocalOperationMethodAllMap = {
+    /**
+     * @example 向本地存储中插入新的消息
+     * @param { Object } msg
+     * @returns {Promise<{} | string>}
+     */
+    InsertMessage(msg) {
+        return new Promise((res, rej) => {
+            USER_MESSAGE.insert({ ...msg }, (err, newDoc) => {
+                err ? rej(err) : res(newDoc)
+            })
+        })
+    },
 
-/**
- * @example 查找对应账户的消息记录
- * @param { String } account
- * @returns { Promise<Array>}
- */
-function findHistoryAccountMessage(account) {
-    return new Promise((res, rej) => {
-        // 查询我发给他的 或 他发给我的
-        DB.message.find(
-            {
-                $or: [{ from: account }, { to: account }]
-            },
-            (err, docs) => {
-                if (err) {
-                    rej(err)
-                } else if (docs.length) {
-                    // 按时间进行排序
-                    docs.sort((a, b) => a.date - b.date)
-                    res(docs)
-                } else {
-                    res([])
+    /**
+     * @example 查找对应账户的消息记录
+     * @param {String} account
+     * @returns {Promise<[{}] | string>}
+     */
+    FindCorrespondAccountMessage(account) {
+        return new Promise((res, rej) => {
+            // 查询我发给他的 或 他发给我的
+            USER_MESSAGE.find(
+                {
+                    $or: [{ from: account }, { to: account }]
+                },
+                (err, docs) => {
+                    // 如果存在就按时间进行排序
+                    err ? rej(err) : res(docs ? docs.sort((a, b) => a.date - b.date) : [])
                 }
-            }
-        )
-    })
-}
+            )
+        })
+    },
 
-/**
- * @example 修改本地的消息卡片，如果消息卡片不存在则插入新的消息卡片
- * @param { String } insertMessageCardAccount
- * @param { Object } msg
- * @returns
- */
-function updateOrInsertMessageCard(insertMessageCardAccount, msg) {
-    return new Promise((res, rej) => {
-        updateMessageCard(insertMessageCardAccount, msg)
-            .then(doc => {
-                console.log("卡片已修改")
-                res(doc)
-            })
-            .catch(err => {
-                insertMessageCard(insertMessageCardAccount, msg)
-                    .then(doc => {
-                        res(doc)
-                    })
-                    .catch(err => {
-                        rej(err)
-                    })
-                console.error(err)
-            })
-    })
-}
+    /**
+     * @example 删除某条消息记录
+     * @param {String} id
+     * @returns {Promise<number | string>}
+     */
+    RemoveMessage(id) {
+        return new Promise((res, rej) => {
+            USER_MESSAGE.remove(
+                {
+                    _id: id
+                },
+                {},
+                (err, numRemoved) => {
+                    err ? rej(err) : res(numRemoved)
+                }
+            )
+        })
+    },
 
-/**
- * @example 修改本地的消息卡片
- * @param { String } insertMessageCardAccount
- * @param { String } msg
- * @returns
- */
-function updateMessageCard(insertMessageCardAccount, { content, date }) {
-    return new Promise((res, rej) => {
-        // 更新该好友消息卡片的消息记录
-        DB.messageCards.update(
-            {
-                // 如果这条消息是我发送出去的，则修改接收方在本地的消息卡片
-                account: insertMessageCardAccount
-            },
-            {
-                $set: {
+    /**
+     * @example 获取本地存储的消息卡片列表
+     * @returns {Promise<[] | string>}
+     */
+    GetChatList() {
+        return new Promise((res, rej) => {
+            USER_CHATLIST.find({}, (err, docs) => {
+                err ? rej(err) : res(docs)
+            })
+        })
+    },
+
+    /**
+     * @example 添加消息卡片
+     * @returns {Promise<{} | string>}
+     */
+    InsterChatCard({ account, avatar, uname, msg: { date, content, _id: mid } }) {
+        return new Promise((res, rej) => {
+            USER_CHATLIST.insert(
+                {
+                    account,
+                    avatar,
+                    uname,
                     msg: {
-                        content: content ?? "",
-                        date: date ?? Date.now()
+                        date,
+                        content,
+                        mid
                     }
+                },
+                (err, newDoc) => {
+                    err ? rej(err) : res(newDoc)
                 }
-            },
-            {
-                multi: false,
-                returnUpdatedDocs: true
-            },
-            (err, ret, newDoc) => {
-                // 查询得到并且修改成功
-                err || ret === 1 ? res(newDoc) : rej(err)
-            }
-        )
-    })
-}
+            )
+        })
+    },
 
-/**
- * @example 向本地存储的消息卡片列表中存储新的消息卡片
- * @param { String } insertMessageCardAccount
- * @param { String } param1
- * @returns
- */
-function insertMessageCard(insertMessageCardAccount, { content, date }) {
-    return new Promise((res, rej) => {
-        // 先查询本地好友列表查看有没有该条消息的来源用户
-        DB.friends.findOne(
-            {
-                account: insertMessageCardAccount
-            },
-            async (err, doc) => {
-                if (err) {
-                    console.error(err)
-                }
-                console.log(doc)
-                // 查询不到用户
-                if (!doc) {
-                    let SearchUserDate = null
-                    try {
-                        // 本地查询不到则到服务器查询
-                        SearchUserDate = await SearchUser(insertMessageCardAccount)
-                        SearchUserDate = SearchUserDate.user
-                        doc = {
-                            name: SearchUserDate.name,
-                            account: SearchUserDate.account,
-                            avatar: SearchUserDate.avatar
-                        }
-                    } catch (err) {
-                        console.error(err)
-                    }
-                }
-                // 添加消息卡片
-                DB.messageCards.insert(
-                    {
-                        ...doc,
+    /**
+     * @example 修改卡片
+     * @returns {Promise<{} | string>}
+     */
+    UpdateChatCard(account, { date, content, _id: mid }) {
+        return new Promise((res, rej) => {
+            USER_CHATLIST.update(
+                {
+                    account
+                },
+                {
+                    $set: {
                         msg: {
+                            date,
                             content,
-                            date
+                            mid
                         }
-                    },
-                    (err, doc) => {
-                        err ? rej(err) : res(doc)
                     }
-                )
-            }
-        )
-    })
-}
-
-/**
- * @example 获取本地存储的消息卡片列表
- * @returns { Promise<[]> }
- */
-function getLocalMessageCard() {
-    return new Promise((res, rej) => {
-        DB.messageCards.find({}, (err, docs) => {
-            err ? rej(err) : res(docs)
+                },
+                {
+                    upsert: true
+                },
+                (err, numReplaced, upsert) => {
+                    err
+                        ? rej(err)
+                        : res({
+                              numReplaced,
+                              upsert
+                          })
+                }
+            )
         })
-    })
-}
-
-/**
- * @example 查询是否存在消息卡片
- * @returns { Promise<Boolean> }
- */
-function findLocalMessageCard(account) {
-    return new Promise((res, rej) => {
-        DB.messageCards.findOne({ account }, (err, doc) => {
-            err ? rej(err) : res(doc)
-        })
-    })
-}
-
-/**
- * @example 查找本地存储的好友列表
- * @returns { Promise<[]> }
- */
-function findLocalFriends() {
-    return new Promise((res, rej) => {
-        DB.friends.find({}, (err, docs) => {
-            err ? rej(err) : res(docs)
-        })
-    })
-}
-
-/**
- * @example 向本地好友列表插入新的好友记录
- * @param { Object } friend
- */
-function insertFriend(friend) {
-    DB.friends.insert(friend)
-}
-
-/**
- * @exampl 向本地好友列表中查找特点的好友记录
- * @param { String } account
- * @returns { Promise<[]> }
- */
-function findLocalFriend(account) {
-    return new Promise((res, rej) => {
-        DB.friends.findOne({ account }, { AddTime: 1, _id: 0 }, (err, doc) => {
-            err ? rej(err) : res(doc)
-        })
-    })
-}
-
-module.exports = {
-    insertMessage,
-    findHistoryAccountMessage,
-    getLocalMessageCard,
-    updateOrInsertMessageCard,
-    insertMessageCard,
-    findLocalMessageCard,
-    insertFriend,
-    findLocalFriends,
-    findLocalFriend
+    }
 }
