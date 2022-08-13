f97aec3806dd325882cd995dd02b2398fe3cded9
Lao-Qi <2231675321@qq.com>
2022-08-13 14:46:13 +0800

    实现软件标题组件UI

--- src/app-render/components/title/linux-title.vue
+++ src/app-render/components/title/linux-title.vue
@@ -1,49 +1,129 @@
 <template>
-    <div id="logo">
-        <img src="../../assets/icons/tibook-login.svg" alt="题书" />
+    <div class="isolation-box">
+        <div id="logo">
+            <img src="../../assets/img/tibook-login.svg" alt="题书" />
+        </div>
+        <div id="search-box" v-if="renderSearchBox">
+            <div class="search-icon">
+                <people-search></people-search>
+            </div>
+            <input type="text" placeholder="搜索" class="search-input" />
+        </div>
     </div>
-    <p></p>
+    <p class="window-drag-area"></p>
     <div class="window-controls-container">
-        <div class="window-icon window-minimize" @click="() => TIBOOK.send('window-minimize')"></div>
-        <div class="window-icon" :class="isMaximize ? 'window-unmaximize' : 'window-maximize'" ref="maximizeBtn" @click="() => TIBOOK.send('window-maximize')"></div>
-        <div class="window-icon window-close" @click="() => TIBOOK.send('window-close')"></div>
+        <div @click="() => TIBOOK.send('window-minimize')" class="window-minimize">
+            <minus></minus>
+        </div>
+        <div @click="() => TIBOOK.send('window-maximize')" class="window-maximize">
+            <off-screen v-if="isMaximize"></off-screen>
+            <full-screen v-else></full-screen>
+        </div>
+        <div @click="() => TIBOOK.send('window-close')" class="window-close">
+            <close></close>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, watch, inject } from "vue"
+import { PeopleSearch, Close, FullScreen, OffScreen, Minus } from "@icon-park/vue-next"
 
 const TIBOOK = window.TIBOOK
+
+const renderSearchBox = ref(false)
 const isMaximize = ref(false)
+
 TIBOOK.on("window-maximize", () => (isMaximize.value = true))
 TIBOOK.on("window-unmaximize", () => (isMaximize.value = false))
+
+watch(
+    () => inject("render").loginUser,
+    value => (renderSearchBox.value = value),
+    { immediate: true }
+)
 </script>
 
 <style scoped lang="less">
-#logo {
-    width: 30px;
+.isolation-box {
+    display: flex;
     height: 100%;
+    margin-top: 5px;
 
-    img {
-        width: 100%;
-        height: 100%;
+    #logo {
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        margin-left: 5px;
+
+        margin-right: 25px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    #search-box {
+        display: flex;
+        width: 200px;
+        height: 30px;
+        padding: 2px;
+        padding-right: 20px;
+        align-items: center;
+        border-radius: 20px;
+        box-shadow: var(--box-inset-show);
+        background-color: var(--input-box-background-color);
+
+        .search-icon {
+            display: inline-block;
+            width: 25px;
+            height: 25px;
+            line-height: 25px;
+            padding-left: 5px;
+            margin-right: 10px;
+            color: #000;
+        }
+
+        .search-input {
+            width: 150px;
+            height: 30px;
+            font-size: 15px;
+            box-sizing: border-box;
+            border: none;
+            background-color: transparent;
+            outline: none;
+
+            &::placeholder {
+                color: #8f8f8f;
+            }
+        }
     }
 }
 
-p {
+.window-drag-area {
     flex: 1;
-    font-family: Haettenschweiler, "Arial Narrow Bold", sans-serif;
-    font-size: 16px;
-    letter-spacing: 1px;
     -webkit-app-region: drag;
 }
 
 .window-controls-container {
-    display: flex;
+    width: auto;
+    height: 30px;
 }
 
 .window-controls-container div {
-    width: 46px;
-    height: 100%;
+    display: inline-block;
+    width: 50px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+}
+.window-controls-container div:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+.window-controls-container .window-close:hover {
+    background-color: #e81123;
+    color: #fff;
 }
 </style>
--- src/app-render/components/title/mac-title.vue
+++ src/app-render/components/title/mac-title.vue
@@ -1,57 +1,117 @@
 <template>
     <div id="window-controls-container">
-        <div class="window-controls" id="window-closure" @click="() => TIBOOK.send('window-close')"></div>
-        <div class="window-controls" id="window-minimize" @click="() => TIBOOK.send('window-minimize')"></div>
-        <div class="window-controls" id="window-maximize" @click="() => TIBOOK.send('window-maximize')"></div>
+        <div class="window-controls window-close" @click="() => TIBOOK.send('window-close')"></div>
+        <div class="window-controls window-minimize" @click="() => TIBOOK.send('window-minimize')"></div>
+        <div class="window-controls window-maximize" @click="() => TIBOOK.send('window-maximize')"></div>
     </div>
-    <p></p>
+    <div class="window-drag-area">
+        <div id="search-box" v-if="renderSearchBox">
+            <div class="search-icon">
+                <people-search></people-search>
+            </div>
+            <input type="text" placeholder="搜索" class="search-input" />
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { watch, inject, ref } from "vue"
+import { PeopleSearch } from "@icon-park/vue-next"
 const TIBOOK = window.TIBOOK
+
+const renderSearchBox = ref(false)
+
+watch(
+    () => inject("render").loginUser,
+    value => (renderSearchBox.value = value),
+    { immediate: true }
+)
 </script>
 
 <style scoped lang="less">
 #window-controls-container {
     display: flex;
-    padding-left: 10px;
+    margin-left: 1px;
+    height: 30px;
 
     .window-controls {
-        display: flex;
-        width: 27px;
-        height: 100%;
-        align-items: center;
-        justify-content: center;
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        text-align: center;
+        line-height: 33px;
 
         &::before {
             content: "";
-            display: block;
+            display: inline-block;
             width: 18px;
             height: 18px;
             cursor: pointer;
+            border-radius: 30px;
             box-sizing: border-box;
-            border-radius: 10px;
+            background-color: rgba(0, 0, 0, 0.3);
         }
     }
 
-    #window-closure::before {
+    .window-close:hover::before {
         border: 2px solid rgb(231, 78, 68);
         background-color: rgb(253, 93, 83);
     }
 
-    #window-maximize::before {
+    .window-maximize:hover::before {
         border: 2px solid rgb(39, 168, 75);
         background-color: rgb(26, 199, 74);
     }
 
-    #window-minimize::before {
+    .window-minimize:hover::before {
         border: 2px solid rgb(228, 164, 45);
         background-color: rgb(255, 181, 43);
     }
 }
 
-p {
+.window-drag-area {
+    display: flex;
+    justify-content: center;
     flex: 1;
+    text-align: center;
     -webkit-app-region: drag;
+
+    #search-box {
+        display: flex;
+        width: 200px;
+        height: 30px;
+        padding: 2px;
+        padding-right: 20px;
+        margin-top: 5px;
+        align-items: center;
+        border-radius: 20px;
+        box-shadow: var(--box-inset-show);
+        background-color: var(--input-box-background-color);
+        -webkit-app-region: no-drag;
+
+        .search-icon {
+            display: inline-block;
+            width: 25px;
+            height: 25px;
+            line-height: 25px;
+            padding-left: 5px;
+            margin-right: 10px;
+            color: #000;
+        }
+
+        .search-input {
+            width: 150px;
+            height: 30px;
+            font-size: 15px;
+            box-sizing: border-box;
+            border: none;
+            background-color: transparent;
+            outline: none;
+
+            &::placeholder {
+                color: #8f8f8f;
+            }
+        }
+    }
 }
 </style>
