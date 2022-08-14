bc731d8fcf43efd3dfc3f431e211f0c436a11db3
Lao-Qi <2231675321@qq.com>
2022-08-15 00:03:19 +0800

    开始清除element-plus模块

--- src/app-render/main.js
+++ src/app-render/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue"
 import { Router } from "./router/index"
-import { ElTooltip, ElNotification } from "element-plus"
-import "element-plus/dist/index.css"
+// import { ElTooltip, ElNotification } from "element-plus"
+// import "element-plus/dist/index.css"
 import "@icon-park/vue-next/styles/index.css"
 import App from "./App.vue"
 
 const app = createApp(App)
 app.use(Router)
-app.use(ElTooltip)
-app.use(ElNotification)
+// app.use(ElTooltip)
+// app.use(ElNotification)
 app.mount("#app")
