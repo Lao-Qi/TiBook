53696b8f4139714998e5670466e7e73b60665de3
Lao-Qi <2231675321@qq.com>
2022-09-12 20:08:36 +0800

    实现项目的打包

---
+++ build/app-electron-build.js
@@ -0,0 +1,71 @@
+// const { spawn } = require("child_process")
+
+// let buildOS = ""
+
+// for (const [_, arg] of Object.entries(process.argv)) {
+//     if (arg.includes("--os")) {
+//         buildOS = `${arg.includes("w") ? "-w" : ""} ${arg.includes("m") ? "-m" : ""} ${arg.includes("l") ? "-l" : ""}`
+//     }
+// }
+
+// const buildElectronProcess = spawn(`electron-builder --config='./electron-builder.js' --projectDir='./dist' ${buildOS}`)
+
+// buildElectronProcess.on("message", msg => {
+//     console.log(msg)
+// })
+
+// buildElectronProcess.on("error", err => {
+//     console.error(err)
+// })
+
+const { build } = require("electron-builder")
+build({
+    config: {
+        productName: "题书",
+        appId: "com.tibook.app",
+        copyright: "Copyright © 2022 - present WangLaoQi(https://github.com/Lao-Qi)",
+        files: ["./app-main/**/*", "./app-render/**/*"],
+        directories: {
+            output: "../app-dist"
+        },
+        dmg: {
+            window: {
+                x: 100,
+                y: 100,
+                width: 500,
+                height: 300
+            }
+        },
+        nsis: {
+            oneClick: false,
+            installerIcon: "./public/icon-256x256.ico",
+            installerHeaderIcon: "./public/icon-256x256.ico",
+            uninstallerIcon: "./public/icon-uninstall-256x256.ico",
+            uninstallDisplayName: "卸载题书",
+            allowElevation: true,
+            createDesktopShortcut: true,
+            createStartMenuShortcut: true,
+            allowToChangeInstallationDirectory: true
+        },
+        win: {
+            icon: "./public/icon-256x256.ico",
+            target: [
+                {
+                    target: "nsis",
+                    arch: ["ia32"]
+                }
+            ]
+        },
+        mac: {
+            icon: "./public/icns-256x256.icns",
+            target: "dmg"
+        },
+        linux: {
+            icon: "./public/icon-256x256.ico",
+            target: "deb"
+        }
+    },
+    projectDir: "./dist"
+}).catch(err => {
+    console.error(err)
+})
--- build/index.js
+++ build/index.js
@@ -2,7 +2,6 @@
 
 const { readFileSync, writeFileSync, cpSync, rmSync, mkdirSync } = require("fs")
 const { join } = require("path")
-// const builder = require("electron-builder")
 
 /**
  * 先打包渲染资源后打包主资源，顺序不能颠倒
@@ -18,129 +17,83 @@ const { join } = require("path")
 
 // 使用立即执行函数的方式可以用来实现终止文件执行的功能
 ;(() => {
-    // 需要按需打包文件
-    if (process.argv[2]) {
-        const buildMod = {}
-        switch (process.argv[2]) {
-            case "main":
-                buildMod.dirInDist = "app-main"
-                buildMod.operationFile = "./app-main-build"
-                break
-            case "render":
-                buildMod.dirInDist = "/app-render"
-                buildMod.operationFile = "./app-render-build"
-                break
-            default:
-                return
+    const build_operates = {
+        init_dist: () => {
+            try {
+                console.log("初始化dist文件夹 ...")
+                rmSync(join(__dirname, "./dist"), { recursive: true })
+                mkdirSync(join(__dirname, "./dist"))
+                console.log("初始化dist文件夹 - 成功")
+            } catch (err) {
+                console.log("初始化dist文件夹 - 失败")
+                throw err
+            }
+        },
+        cp_public: () => {
+            try {
+                console.log("拷贝public目录 ...")
+                cpSync(join(__dirname, "../public"), join(__dirname, "./dist/public"), { recursive: true })
+                console.log("拷贝public目录 - 成功")
+            } catch (err) {
+                console.log("拷贝public资源 - 失败")
+                throw err
+            }
+        },
+        generate_package: () => {
+            /**
+             * 在打包好了的dist文件夹内部中添加package.json文件，其中的信息来自跟目录下的app.deploy.json
+             *
+             * dist将作为软件最终的资源被打包进electron应用的app.asar中
+             */
+            try {
+                console.log("生成package.json ...")
+                const APP_DEPLOY = JSON.parse(readFileSync(join(__dirname, "../app.deploy.json")))
+                const APP_electron_version = JSON.parse(readFileSync(join(__dirname, "../package.json"))).devDependencies.electron
+                // 配置打包好的资源的入口文件
+                APP_DEPLOY["main"] = "./app-main/main.js"
+
+                APP_DEPLOY["devDependencies"] = {
+                    electron: APP_electron_version
+                }
+
+                writeFileSync(join(__dirname, "./dist/package.json"), JSON.stringify(APP_DEPLOY))
+                console.log("生成package.json - 成功")
+            } catch (err) {
+                console.log("生成package.json - 失败")
+                throw err
+            }
+        },
+        render_build: () => {
+            rmSync(join(__dirname, "./dist/app-render"), { recursive: true })
+            mkdirSync(join(__dirname, "./dist/app-render"))
+            require("./app-render-build")
+        },
+        main_build: () => {
+            rmSync(join(__dirname, "./dist/app-main"), { recursive: true })
+            mkdirSync(join(__dirname, "./dist/app-render"))
+            require("./app-main-build")
+        },
+        electron_build: () => {
+            require("./app-electron-build")
         }
-        rmSync(join(__dirname, `./dist/${buildMod.dirInDist}`), { recursive: true })
-        mkdirSync(join(__dirname, `./dist/${buildMod.dirInDist}`))
-        require(buildMod.operationFile)
-        return
     }
 
-    try {
-        console.log("初始化dist文件夹 ...")
-        rmSync(join(__dirname, "./dist"), { recursive: true })
-        mkdirSync(join(__dirname, "./dist"))
-        console.log("初始化dist文件夹 - 成功")
-    } catch (err) {
-        console.log("初始化dist文件夹 - 失败")
-        throw err
-    }
+    if (process.argv.length >= 2) {
+        process.argv.shift()
+        process.argv.shift()
 
-    require("./app-render-build")
-    require("./app-main-build")
-
-    try {
-        console.log("拷贝public目录 ...")
-        cpSync(join(__dirname, "../public"), join(__dirname, "./dist/public"), { recursive: true })
-        console.log("拷贝public目录 - 成功")
-    } catch (err) {
-        console.log("拷贝public资源 - 失败")
-        throw err
-    }
-
-    /**
-     * 在打包好了的dist文件夹内部中添加package.json文件，其中的信息来自跟目录下的app.deploy.json
-     *
-     * dist将作为软件最终的资源被打包进electron应用的app.asar中
-     */
-    try {
-        console.log("生成package.json ...")
-        const APP_DEPLOY = JSON.parse(readFileSync(join(__dirname, "../app.deploy.json")))
-        // 配置打包好的资源的入口文件
-        APP_DEPLOY["main"] = "./app-main/main.js"
-        writeFileSync(join(__dirname, "./dist/package.json"), JSON.stringify(APP_DEPLOY))
-        console.log("生成package.json - 成功")
-    } catch (err) {
-        console.log("生成package.json - 失败")
-        throw err
+        for (const [_, operateName] of Object.entries(process.argv)) {
+            const operate = build_operates[operateName]
+            if (operate) {
+                operate()
+            }
+        }
+    } else {
+        build_operates.init_dist()
+        build_operates.main_build()
+        build_operates.render_build()
+        build_operates.cp_public()
+        build_operates.generate_package()
+        build_operates.electron_build()
     }
 })()
-
-/**
- * 通过electron-builder打包应用
- */
-
-// builder
-//     .build({
-//         config: {
-//             productName: "题书", // 软件名称
-//             appId: "com.tibook.app", // 软件id
-//             copyright: "Copyright © 2022 - present WangLaoQi(https://github.com/Lao-Qi)", // 软件版权
-//             files: ["../render-dist/**/*", "../electron-dist/**/*"], // 软件打包资源文件
-//             directories: {
-//                 output: "electron_dist", // 软件打包后输出的文件夹
-//             },
-//             dmg: {
-//                 // dmg macOS系统上的安装包格式
-//                 window: {
-//                     // 安装包窗口
-//                     x: 100,
-//                     y: 100,
-//                     width: 500,
-//                     height: 300,
-//                 },
-//             },
-//             // nsis windows系统上的一个打包配置程序，支持打包出exe可执行文件
-//             nsis: {
-//                 oneClick: false, // 是否一键安装
-//                 installerIcon: "/dist/icon-256x256.ico", // 软件图标
-//                 installerHeaderIcon: "/dist/icon-256x256.ico", // 安装程序的标题图标
-//                 uninstallerIcon: "/dist/icon-uninstall-256x256.ico", // 卸载软件程序的图标
-//                 uninstallDisplayName: "卸载题书", // 卸载软件程序名称
-//                 allowElevation: true, // 让用户不需要使用管理员模式启动软件
-//                 createDesktopShortcut: true, // 创建软件快捷打开方式
-//                 createStartMenuShortcut: true, // 创建菜单快捷打开方式
-//                 allowToChangeInstallationDirectory: true, // 允许用户更改软件的安装位置
-//             },
-//             win: {
-//                 // windows系统配置
-//                 icon: "/dist/icon-64x64.ico", // 软件图标
-//                 target: [
-//                     {
-//                         target: "nsis", // 软件打包配置程序 类型打包额驱动
-//                         arch: "ia32", // 将软件打包成32位程序
-//                     },
-//                 ],
-//             },
-//             mac: {
-//                 // macOS系统配置
-//                 icon: "/dist/icons-64x64.icns", // 软件图标
-//                 target: "dmg", // 打包驱动
-//             },
-//             linux: {
-//                 // linux系统配置
-//                 icon: "/dist/icon-64x64.ico", // 软件图标
-//                 target: "deb", // 打包驱动
-//             },
-//         },
-//     })
-//     .then(() => {
-//         console.log("项目打包成功")
-//     })
-//     .catch(err => {
-//         console.log("项目打包失败")
-//         console.error(err)
-//     })
